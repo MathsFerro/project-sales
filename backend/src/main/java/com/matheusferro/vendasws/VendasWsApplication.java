24d835f875b0f21e7ccedd01b0e206bc464bed80
Matheus Ferro <matheusferroraimundo@gmail.com>
2021-09-07 21:14:50 -0300

    f c

---
+++ backend/src/main/java/com/matheusferro/vendasws/VendasWsApplication.java
@@ -0,0 +1,13 @@
+package com.matheusferro.vendasws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasWsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matheusferro/vendasws/VendasWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusferro.vendasws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
