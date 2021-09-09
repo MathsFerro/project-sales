19fcb49f7cd83edb736d797c1637d2ad636352fb
Matheus Ferro <matheusferroraimundo@gmail.com>
2021-09-08 23:17:06 -0300

    Security config

--- backend/src/main/java/com/matheusferro/vendasws/VendasWsApplication.java
+++ backend/src/main/java/com/matheusferro/vendasws/VendasWsApplication.java
@@ -3,6 +3,11 @@ package com.matheusferro.vendasws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class VendasWsApplication {
 
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -12,7 +12,6 @@ export function DonutChart() {
     }
   }
 
-
   return (
     <>
       <Chart 
